3dc66df47070beda5537e6d8492fc3671ea9836a
Sven Günther <sven.guenther@it-agile.de>
2015-09-14 17:33:09 +0200

    added xcode project

---
+++ XMLParser/XMLParser.h
@@ -0,0 +1,19 @@
+//
+//  XMLParser.h
+//  XMLParser
+//
+//  Created by Sven Günther on 14.09.15.
+//  Copyright © 2015 it-agile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLParser.
+FOUNDATION_EXPORT double XMLParserVersionNumber;
+
+//! Project version string for XMLParser.
+FOUNDATION_EXPORT const unsigned char XMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLParser/PublicHeader.h>
+
+
